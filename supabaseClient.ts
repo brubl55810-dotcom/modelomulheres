038064e80a3c20a3d2854494f6aa85765260b631
brubl55810-dotcom <brubl55810@gmail.com>
2026-02-12 16:04:35 -0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://bdimsucrfuzdvfshsnry.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_f2EOzMlW6IFgBBbUZ7HtiA_5d_PVPIh';
+const SUPABASE_URL = 'https://dootxtmvyqxhnmprgqdq.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_aZrPqWq5Qb41RXtogrJ7vQ_NjM1FIB1';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
